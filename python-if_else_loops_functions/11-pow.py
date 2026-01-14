d68d53e09b43d541726e13e40f081f4d4d337e8b
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2026-01-14 13:33:22 +0000

    add: function that computes a to the power of b and return the value

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """Return a raised to the power of b.
+
+    Args:
+        a (int): The base integer.
+        b (int): The exponent integer.
+    Returns:
+        int: The result of a raised to the power of b.
+    """
+    return a ** b
