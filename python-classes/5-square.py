3b354b3b33f86ba01f944a176dc727f1288bb9e6
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2026-01-29 14:43:42 +0000

    add : a class Square that defines a square by: (based on 4-square.py)

---
+++ python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with size validation, area calculation,
+and a method to print the square using '#' characters.
+"""
+
+
+class Square:
+    """
+    Represents a square with size validation, area calculation, and printing.
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("value must be an integer")
+        if value < 0:
+            raise ValueError("value must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square using '#' characters."""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
