e91ddd4c7424ae372087e1b98276b80af6874391
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2026-01-29 14:38:59 +0000

    add :  a class Square that defines a square by: (based on 1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with a size attribute that
+includes validation.
+"""
+
+
+class Square:
+    """
+    Represents a square with size validation.
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance with a given size.
+        Args:
+            size (int): The size of the square (default is 0).
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is negative.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
