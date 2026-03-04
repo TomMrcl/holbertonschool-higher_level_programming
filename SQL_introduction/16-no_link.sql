ec8e2a369853586df19a19320fc261b4d297c922
TomMrcl <tomarchal02@gmaiL.com>
2026-03-04 09:39:40 +0000

    feat: a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- 16. No link
+-- Lists all records of second_table where name is not NULL or empty
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
