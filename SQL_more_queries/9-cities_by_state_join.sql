942fe1f427e4e7d2cf8a6cd871b63ce7fb146343
TomMrcl <tomarchal02@gmaiL.com>
2026-03-04 10:42:23 +0000

    feat:  a script that lists all cities contained in the database hbtn_0d_usa.

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- 9. Cities by state
+-- Lists all cities with their corresponding state
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
