ba1916609d64a6f3abddabb68e1f21db04761938
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2026-03-06 13:02:03 +0000

    fix:  script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
+"""List all states from the database."""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """Run the SQL query and print results."""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state = sys.argv[4]
+    state_searched = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database
+        db=database,
     )
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state,))
+    cursor.execute("""
+        SELECT * FROM states
+        WHERE name = %s
+        ORDER BY id ASC;
+        """, (state_searched,))
 
-    results = cursor.fetchall()
+    states = cursor.fetchall()
 
-    for row in results:
-        print(row)
+    for state in states:
+        print(state)
 
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
